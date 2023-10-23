ed8073ced2b7d9c00f8ca56246455ac517ed39ef
koyuncu5362 <koyuncu.m@hotmail.com>
2023-10-23 16:10:13 +0300

    Long Value Type

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿
 Console.WriteLine("Hello World");
 Console.ReadKey();
+
+//-----------------Value Types-------------
+
+//int
+int number1 = 10;
+Console.WriteLine("Number1 is {0}",number1);
+Console.ReadLine();
+
+//long
+long number2 = 915555555555555555;
+Console.WriteLine("Number1 is {0}", number2);
+Console.ReadLine();
