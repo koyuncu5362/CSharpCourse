564c5346695bca537b4d6e4cae93ab323a456878
koyuncu5362 <koyuncu.m@hotmail.com>
2023-10-23 16:12:22 +0300

    Byte Value Type

--- Program.cs
+++ Program.cs
@@ -11,10 +11,15 @@ Console.ReadLine();
 
 //long
 long number2 = 915555555555555555;
-Console.WriteLine("Number1 is {0}", number2);
+Console.WriteLine("number2 is {0}", number2);
 Console.ReadLine();
 
 //short
 short number3 = 32767;
-Console.WriteLine("Number1 is {0}", number2);
+Console.WriteLine("number3 is {0}", number3);
+Console.ReadLine();
+
+//byte
+byte number4 = 255;
+Console.WriteLine("number4 is {0}", number4);
 Console.ReadLine();
