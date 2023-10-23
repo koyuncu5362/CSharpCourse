aba340b921ba39011d9ab074bd948dadfd37a86e
koyuncu5362 <koyuncu.m@hotmail.com>
2023-10-23 16:11:17 +0300

    Short Value Type

--- Program.cs
+++ Program.cs
@@ -13,3 +13,8 @@ Console.ReadLine();
 long number2 = 915555555555555555;
 Console.WriteLine("Number1 is {0}", number2);
 Console.ReadLine();
+
+//short
+short number3 = 32767;
+Console.WriteLine("Number1 is {0}", number2);
+Console.ReadLine();
