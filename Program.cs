23fd02664def3b4aa55fe17f607f693f0b0a11b2
koyuncu5362 <koyuncu.m@hotmail.com>
2023-10-23 17:10:47 +0300

    String Methods

--- Program.cs
+++ Program.cs
@@ -285,3 +285,21 @@ foreach (var item in city)
     
 }
 Console.ReadLine();
+
+//String Methods
+string sentence = "My name is Metin";
+Console.WriteLine(sentence.Length);
+
+var sentence2 = sentence.Clone();
+Console.WriteLine(sentence.EndsWith('n'));
+Console.WriteLine(sentence.StartsWith("My name"));
+Console.WriteLine(sentence.IndexOf("name"));
+Console.WriteLine(sentence.LastIndexOf("name"));
+Console.WriteLine(sentence.Insert(0,"hello, "));
+Console.WriteLine(sentence.Substring(3));
+Console.WriteLine(sentence.Substring(3,5));
+Console.WriteLine(sentence.ToUpper());
+Console.WriteLine(sentence.ToLower());
+Console.WriteLine(sentence.Replace(" ","-"));
+Console.WriteLine(sentence.Remove(2,4));
+Console.ReadLine();
