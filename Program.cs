3326cdb7ea9d36fe498a1b7836417196afeef57b
koyuncu5362 <koyuncu.m@hotmail.com>
2023-10-24 13:12:22 +0300

    Inheritances

--- Program.cs
+++ Program.cs
@@ -468,3 +468,24 @@ IWorker[] workers = new IWorker[3]
     new Robot(),
     new Manager()
 };
+
+// -------------------------------INHERİTANCE------------------------
+
+class Person 
+    {
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+class Customer7: Person
+{
+    public string City { get; set; }
+}
+class Student : Person
+{
+    public string Class { get; set; }
+}
+Person[] persons = new Person[3]
+{
+    new Person{FirstName="Mahmut" },new Customer7{FirstName="Metin" },new Student{FirstName="Koyuncu" }
+};
